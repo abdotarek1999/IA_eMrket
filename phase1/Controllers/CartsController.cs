1fbf17d8288dc887952bca4e98a07b566a47f8f3
Ahmed Radi <ahmedradi743@gmail.com>
2020-05-21 00:52:07 +0200

    Add Carts Controller

---
+++ phase1/Controllers/CartsController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace phase1.Controllers
+{
+    public class CartsController : Controller
+    {
+        // GET: Carts
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult AddToCart(int id)
+        {
+            var cart = new cart();
+            DateTime localDate = DateTime.Now;
+            cart.added_at = localDate;
+            cart.product_Id = id;
+            var cart2 = db.carts.Find(id);
+            if (cart2 != null)
+            {
+                return RedirectToAction("Filter", "products");
+            }
+            else
+            {
+                db.carts.Add(cart1);
+                db.SaveChanges();
+                return RedirectToAction("Filter", "products");
+            }
+        }
+        public ActionResult Remove(int id)
+        {
+            var _cart = db.carts.Find(id);
+            if (_cart != null)
+            {
+                db.carts.Remove(_cart);
+            }
+            db.SaveChanges();
+            return RedirectToAction("Filter", "products");
+        }
+
+    }
+}
